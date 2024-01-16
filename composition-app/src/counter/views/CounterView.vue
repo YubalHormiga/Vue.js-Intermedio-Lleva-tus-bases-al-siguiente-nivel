bad7d1a232a28041a70a1a4ecec391d83aa326bc
YubalHormiga <yubalhh@gmail.com>
2024-01-16 10:15:28 +0100

    finalizado sección 2 y 3

---
+++ composition-app/src/counter/views/CounterView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const counter = ref(5)
+
+// const increase = () => {
+//   counter.value++
+// }
+</script>
+
+<template>
+  <h1>Counter View: {{ counter }}</h1>
+  <button @click="counter++">+1</button>
+</template>
+<style scoped></style>
---
+++ composition-app/src/pokemons/layouts/PokemonLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from '@/shared/components/NavBar.vue'
+import { pokemonRoute } from '../router/index'
+import type { RouterLink } from '../../router/list-routers'
+
+const routeLinks: RouterLink[] =
+  pokemonRoute.children?.map(({ name, path, props }) => {
+    return {
+      name: name?.toString() ?? '',
+      path: path,
+      title: (props as { title: string }).title
+    }
+  }) || []
+</script>
+
+<template>
+  <div>
+    <NavBar :links="routeLinks" is-secondary />
+    <h4>Pokemon Layout</h4>
+    <RouterView />
+  </div>
+</template>
