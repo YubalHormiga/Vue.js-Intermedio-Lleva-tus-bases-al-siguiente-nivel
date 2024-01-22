bdc73d7286ffd520d745177f20aeb6fe8b13af24
YubalHormiga <yubalhh@gmail.com>
2024-01-22 12:12:06 +0100

    finished section 6

---
+++ 02-PiniaApp/src/counter/pages/CounterSetupPage.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useCounterSetupStore } from '@/store/counter-setup';
+import CounterSetupValue from '../components/CounterSetupValue.vue';
+
+
+const counterStore = useCounterSetupStore();
+const { count, squareCount } = storeToRefs( counterStore );
+const { increment , incrementBy } = counterStore;
+
+</script>
+<template>
+    <h1>Pinia Setup</h1>
+    <h4>Counter: {{ count }} </h4>
+    <h4>Square: {{ squareCount }}</h4>
+
+    <br>
+    <button @click="increment">+1</button>
+    <button @click="incrementBy(2)">+2</button>
+    <button @click="incrementBy(5)">+5</button>
+
+    <br>
+    <CounterSetupValue />
+
+</template>
+
+
+<style scoped>
+button {
+    margin-right: 5px;
+}
+</style>
